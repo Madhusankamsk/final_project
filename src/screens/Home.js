e082167dec1c059a196eaaa0c01633782b695c18
Madhusankamsk <72614637+Madhusankamsk@users.noreply.github.com>
2023-01-13 15:18:09 +0530

    set_the_tailwind
    Set the tailwind for project

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button,View } from 'react-native';
 
 export default function Home({navigation}) {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
         <Button title='First' onPress={()=> navigation.navigate("First")}/>
         <Button title='Screen' onPress={()=> navigation.navigate("Second")}/>
         <StatusBar style='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width:70,
-    height:50
-  },
-});
--- src/screens/NavScreens/Second.js
+++ src/screens/NavScreens/Second.js
@@ -1,21 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 
 export default function Second() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Second</Text>
       <StatusBar/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
